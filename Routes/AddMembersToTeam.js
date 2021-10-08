4f80604577cc144d969eef42a05af5dcc0f6b4ca
Harsh Sharma <harshrajsharma.2003@gmail.com>
2021-10-08 14:13:26 +0530

    Members Add Is Working

--- Routes/AddMembersToTeam.js
+++ Routes/AddMembersToTeam.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 
 module.exports= function AddMembersToTeam(req, res){
 
-    mongoose.model("TeamMembers").findOneAndUpdate({year: req.body.year}, {$push:{members: {name: "Applee", role: "Applee"}}} , {new: true}, function(err, response){
+    mongoose.model("TeamMembers").findOneAndUpdate({year: req.body.year}, {$push:{members: {name: req.body.name, role: req.body.role}}} , {new: true}, function(err, response){
         res.send(response);
     });
     console.log(req.body);
