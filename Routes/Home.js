32427f1e1e3a60c580ca031ca0e5b952b3483a93
Harsh Sharma <harshrajsharma.2003@gmail.com>
2021-10-14 16:31:59 +0530

    Home Route

--- Routes/Home.js
+++ Routes/Home.js
@@ -1,3 +1,7 @@
 module.exports=function Home(req, res){
-    res.send("Hello Server");
+    res.send(
+        "<h2>Hello</h2>"+
+        "<h1>This Server is created by Harsh</h1>"
+        
+    );
 }
